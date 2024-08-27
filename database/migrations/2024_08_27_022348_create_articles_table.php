fb9a638d79f9ed01741d9e7002b35e2952134ac9
Erinthia16 <erinthiapratiwi167@gmail.com>
2024-08-27 16:16:45 +0700

    pert28 ubah fungsi up

--- database/migrations/2024_08_27_022348_create_articles_table.php
+++ database/migrations/2024_08_27_022348_create_articles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateArticlesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->string('featured_image');
             $table->timestamps();
         });
     }
